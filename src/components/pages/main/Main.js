10cdf78c71fd49972ec78258c3bc666dd278d0cd
Lior Fridman <liorspidy@gmail.com>
2022-12-27 19:12:29 +0200

    added main paragraph and buttons css

--- src/components/pages/main/Main.js
+++ src/components/pages/main/Main.js
@@ -5,6 +5,15 @@ const Main = (props) => {
   return (
     <div>
       <h1 className="title">Welcome to MaintControl</h1>
+      <div className="mainP">
+        <p>
+          A location based application that will help maintenance service
+          company to complete and optimize their routine work using various
+          parameters. This application will help workers to do their job more
+          effective due to knowledge database , work sequence and work
+          documentation.
+        </p>
+      </div>
       <Link className="loginButton" to="login">
         Login Page
       </Link>
