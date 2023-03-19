784d13db7bfadeb236239dca686ae078074d2568
matan avital <matinza.avital@gmail.com>
2023-03-19 02:00:26 +0200

    litle fix to docker init

--- src/Server/docker_postgres_init.sql
+++ src/Server/docker_postgres_init.sql
@@ -5,7 +5,7 @@ company_name VARCHAR(255) NOT NULL UNIQUE
 
 CREATE TABLE IF NOT EXISTS public.dim_users (
   user_id SERIAL PRIMARY KEY,
-  user_name VARCHAR(255) NOT NULL,
+  user_name VARCHAR(255) NOT NULL UNIQUE,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE,
