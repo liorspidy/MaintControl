d77146f829fa0940b0f09bbcbdf949600f499529
matan avital <matinza.avital@gmail.com>
2023-03-24 14:55:01 +0300

    userSettings api

--- src/Server/models/users/administratorUser/getUsers.js
+++ src/Server/models/users/administratorUser/getUsers.js
@@ -5,7 +5,8 @@ getUsers = (decodedToken, query) => {
   return new Promise((resolve, reject) => {
     try {
       result = db.query(`SELECT * FROM dim_users
-                         WHERE company_id = ${decodedToken.companyId}
+                         WHERE company_id = ${decodedToken.companyId} AND
+                         company_id != ${decodedToken.userId}
                          ORDER BY user_id
                          OFFSET ${query.OFFSET}
                          LIMIT ${query.LIMIT}`);
