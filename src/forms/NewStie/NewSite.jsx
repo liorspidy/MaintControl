91c410a26858da2ee864c5f4b7e755df75cbe5da
amitbz1998 <amitbz1998@gmail.com>
2023-03-26 19:33:47 +0300

    add text field for name and description

--- src/forms/NewStie/NewSite.jsx
+++ src/forms/NewStie/NewSite.jsx
@@ -4,9 +4,11 @@ import "./NewSite.css";
 import Modal from "@mui/material/Modal";
 import DrawableMap from "../../components/drawableMap/DrawableMap";
 import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
 const NewSite = ({ isOpen, closeForm }) => {
   const [mapLayers, setMapLayers] = useState([]);
+  // const [siteName, setSiteName] = usestate("");
 
   const saveData = () => {
     console.log("data to save is", { points: mapLayers });
@@ -17,7 +19,28 @@ const NewSite = ({ isOpen, closeForm }) => {
         <div className="new-site-modal-container">
           <div className="new-site-modal-form">
             <h2>New Site Form</h2>
-            <Button variant="contained" onClick={saveData}>Save Data</Button>
+            <div className="new-site-form-body">
+              <TextField
+                id="outlined-basic"
+                label="Site Name"
+                variant="outlined"
+              />
+
+              <TextField
+                id="outlined-multi-basic"
+                label="Site Description"
+                variant="outlined"
+                multiline
+                rows={4}
+              />
+            </div>
+            <Button
+              variant="contained"
+              onClick={saveData}
+              className="new-site-form-button"
+            >
+              Save Data
+            </Button>
           </div>
           <div className="new-site-modal-map">
             <DrawableMap mapLayers={mapLayers} setMapLayers={setMapLayers} />
