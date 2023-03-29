ee3684eded555feb12589a9291d29765c62da95a
matan avital <matinza.avital@gmail.com>
2023-03-29 18:42:23 +0300

    fixed user name unique for each company

--- src/Server/docker_postgres_init.sql
+++ src/Server/docker_postgres_init.sql
@@ -1,46 +1,44 @@
-CREATE TABLE IF NOT EXISTS public.dim_companies(
+/*###########################*/
+/*######## users ############*/
+/*###########################*/
+CREATE TABLE IF NOT EXISTS dim_companies(
   company_id SERIAL PRIMARY KEY,
   company_name VARCHAR(255) NOT NULL UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS public.dim_users (
+CREATE TABLE IF NOT EXISTS dim_users (
   user_id SERIAL PRIMARY KEY,
-  user_name VARCHAR(255) NOT NULL UNIQUE,
+  user_name VARCHAR(255) NOT NULL,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL,
   phone VARCHAR(50) NOT NULL,
   role VARCHAR(50) NOT NULL CHECK (role IN ('maintainance', 'manager', 'administrator')),
-  company_id INTEGER NOT NULL REFERENCES public.dim_companies(company_id)
+  company_id INTEGER NOT NULL REFERENCES dim_companies(company_id)
 );
 
-INSERT INTO public.dim_companies (company_name)
+/*make sure each company can have unique user names
+ but every company can the same user name of diffrent cmpanies*/
+CREATE UNIQUE INDEX idx_dim_users_company_user ON dim_users (company_id, user_name);
+
+INSERT INTO dim_companies (company_name)
 VALUES ('Acme Corporation');
 
-INSERT INTO public.dim_users (
-    user_name,
-    first_name,
-    last_name,
-    email,
-    password,
-    phone,
-    role,
-    company_id
-) VALUES (
-    'tempAdmin',
-    'John',
-    'Doe',
-    'johndoe@example.com',
-    'Password1',
-    '555-1234',
-    'administrator',
-    1
-);
+INSERT INTO dim_companies (company_name)
+VALUES ('HIT');
 
-CREATE TABLE IF NOT EXISTS public.fact_guides (
+INSERT INTO dim_users 
+(user_name, first_name, last_name, email, password, phone, role, company_id) VALUES
+ ('tempAdmin', 'John', 'Doe', 'johndoeACME@example.com', 'Password1', '555-1234', 'administrator', 1),
+ ('tempAdmin', 'John', 'Doe', 'johndoeHIT@example.com', 'Password1', '555-1234', 'administrator', 2);
+
+/*###########################*/
+/*######## guides ###########*/
+/*###########################*/
+CREATE TABLE IF NOT EXISTS fact_guides (
   guide_id SERIAL PRIMARY KEY,
-  guide_title VARCHAR(255) NOT NULL UNIQUE,
+  guide_title VARCHAR(255) NOT NULL,
   guide_content TEXT NOT NULL,
   company_id INTEGER NOT NULL REFERENCES public.dim_companies(company_id)
 );
