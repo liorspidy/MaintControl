1e9761ecc48dea80ce74f132df3cc145b48b22bc
NaveShimoni <nave41098@gmail.com>
2023-05-08 00:17:59 +0300

    showUser page
    Added new page - when you press on a user from the admin page, if no button (edit or delete) is pressed, you will get to showUser page that contains the user details

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import './App.css';
-import Header from './components/header/Header';
-import Login from './components/pages/login/Login';
-import Main from './components/pages/main/Main';
-import { useLocation } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
-import Forgot from './components/pages/login/forgot/Forgot';
-import Task from './components/pages/task/Task';
-import ManagementMap from './components/pages/ManagementMap/ManagementMap';
-import Guides from './components/pages/guides/Guides';
-import AddGuide from './components/pages/guides/addGuide/AddGuide';
-import EditGuide from './components/pages/guides/editGuide/EditGuide';
-import GuideDetails from './components/pages/guides/GuideDetails';
-import CartProvider from './store/CartProvider';
-import Admin from './components/pages/users/admin/Admin';
-import Missions from './components/pages/missions/Missions';
-import NewMission from './components/pages/missions/NewMission';
-import AddUser from './components/pages/users/admin/addUser/AddUser';
-import EditUser from './components/pages/users/admin/editUser/EditUser';
-import EditMission from './components/pages/missions/EditMission';
-import TaskEditForm from './components/pages/task/TaskEditForm';
-import TaskAddForm from './components/pages/task/TaskAddForm';
+import "./App.css";
+import Header from "./components/header/Header";
+import Login from "./components/pages/login/Login";
+import Main from "./components/pages/main/Main";
+import { useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Forgot from "./components/pages/login/forgot/Forgot";
+import Task from "./components/pages/task/Task";
+import ManagementMap from "./components/pages/ManagementMap/ManagementMap";
+import Guides from "./components/pages/guides/Guides";
+import AddGuide from "./components/pages/guides/addGuide/AddGuide";
+import EditGuide from "./components/pages/guides/editGuide/EditGuide";
+import GuideDetails from "./components/pages/guides/GuideDetails";
+import CartProvider from "./store/CartProvider";
+import Admin from "./components/pages/users/admin/Admin";
+import Missions from "./components/pages/missions/Missions";
+import NewMission from "./components/pages/missions/NewMission";
+import AddUser from "./components/pages/users/admin/addUser/AddUser";
+import EditUser from "./components/pages/users/admin/editUser/EditUser";
+import ShowUser from "./components/pages/users/admin/showUser/ShowUser";
+import EditMission from "./components/pages/missions/EditMission";
+import TaskEditForm from "./components/pages/task/TaskEditForm";
+import TaskAddForm from "./components/pages/task/TaskAddForm";
 
 function App() {
-  const location = useLocation().pathname.replace('/', '');
+  const location = useLocation().pathname.replace("/", "");
 
   return (
     <CartProvider>
@@ -53,6 +54,7 @@ function App() {
           <Route path="admin" element={<Admin />} />
           <Route path="admin/addUser" element={<AddUser />} />
           <Route path="admin/editUser/:userId" element={<EditUser />} />
+          <Route path="admin/showUser/:userId" element={<ShowUser />} />
         </Routes>
       </div>
     </CartProvider>
--- src/components/pages/guides/GuideDetails.js
+++ src/components/pages/guides/GuideDetails.js
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 import items from "./guides.json";
 import { Link } from "react-router-dom";
 
-const GuideDetails = (props) => {
+const GuideDetails = () => {
   const { guideId } = useParams();
   const guide = items.find((item) => item.id === parseInt(guideId));
 
@@ -11,7 +11,7 @@ const GuideDetails = (props) => {
       <h3>{guide.name}</h3>
       <p>{guide.description}</p>
       <Link to="../guides">
-        <button className="addGuideBtn">Back</button>
+        <button className="backBtn">Back</button>
       </Link>
     </div>
   );
