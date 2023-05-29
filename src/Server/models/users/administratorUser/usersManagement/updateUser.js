176d99d2db5fb3006fc6790a60d5664bc46a5fac
matan avital <matinza.avital@gmail.com>
2023-05-29 15:50:34 +0300

    update user with location

--- src/Server/models/users/administratorUser/usersManagement/updateUser.js
+++ src/Server/models/users/administratorUser/usersManagement/updateUser.js
@@ -3,37 +3,53 @@ var db = require('../../../../db/index')
 
 updateUser = (data, decodedToken) => {
   return new Promise((resolve, reject) => {
-    try {
-      result = db.query(`UPDATE dim_users 
-                         SET user_name = '${data.user_name}',
-                            first_name = '${data.first_name}',
-                            last_name = '${data.last_name}',
-                            email = '${data.email}',
-                            password = '${data.password}',
-                            phone = '${data.phone}',
-                            role = '${data.role}' 
-                            WHERE user_id = ${data.user_id} AND 
-                            company_id = ${decodedToken.companyId}`)
-
-      result.then(() => {
-          resolve({
-            httpCode: 200,
-            answer: "updated user succesfuly"
-          })
-        })
-        .catch((err) => {
-          reject({
-            httpCode: 500,
-            answer: `Error during updating user: ${err}`
-          })
-        })
-    } catch (error) {
-      reject({
-        httpCode: 500,
-        answer: "Internal server error"
+    db.query('BEGIN')
+      .then(() => {
+        return db.query(`SELECT location_id FROM dim_users WHERE user_id = $1 AND company_id = $2`, 
+          [data.user_id, decodedToken.companyId])
       })
-    }
-  })
+      .then((res) => {
+        const locationId = res.rows[0].location_id;
+        return db.query(`UPDATE dim_locations 
+          SET address_name = $1,
+              city = $2,
+              country = $3,
+              latitude = $4,
+              longitude = $5,
+              zone_name = $6
+          WHERE location_id = $7`, 
+          [data.location.address_name, data.location.city, data.location.country, data.location.latitude, data.location.longitude, data.location.zone_name, locationId])
+      })
+      .then(() => {
+        return db.query(`UPDATE dim_users 
+          SET user_name = $1,
+              first_name = $2,
+              last_name = $3,
+              email = $4,
+              password = $5,
+              phone = $6,
+              role = $7
+          WHERE user_id = $8 AND 
+          company_id = $9`, 
+          [data.user_name, data.first_name, data.last_name, data.email, data.password, data.phone, data.role, data.user_id, decodedToken.companyId])
+      })
+      .then(() => {
+        return db.query('COMMIT')
+      })
+      .then(() => {
+        resolve({
+          httpCode: 200,
+          answer: "Updated user successfully"
+        });
+      })
+      .catch((err) => {
+        db.query('ROLLBACK');
+        reject({
+          httpCode: 500,
+          answer: `Error during updating user: ${err}`
+        });
+      });
+  });
 }
 
 module.exports = {
